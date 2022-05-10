f47ed2ed654afc18ba4f37202f18f238203a16bc
Gary Thung <garythung@gmail.com>
2022-05-09 19:06:00 -0700

    ignore empty function ts eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,13 @@
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
+  rules: {
+    "@typescript-eslint/no-empty-function": 1,
+  },
 };
